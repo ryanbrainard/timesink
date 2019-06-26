3637598eaa4748777d4c0cdd2f08ec97be5a188f
Ryan Brainard <ryan@ryanbrainard.com>
2019-06-26 17:17:16 +0900

    add todo example

--- querier.go
+++ querier.go
@@ -4,6 +4,40 @@ import (
 	"github.com/graphql-go/graphql"
 )
 
+type Todo struct {
+	ID   string `json:"id"`
+	Text string `json:"text"`
+	Done bool   `json:"done"`
+}
+
+var TodoList []Todo
+
+func init() {
+	todo1 := Todo{ID: "a", Text: "A todo not to forget", Done: false}
+	todo2 := Todo{ID: "b", Text: "This is the most important", Done: false}
+	todo3 := Todo{ID: "c", Text: "Please do this or else", Done: false}
+	TodoList = append(TodoList, todo1, todo2, todo3)
+}
+
+// define custom GraphQL ObjectType `todoType` for our Golang struct `Todo`
+// Note that
+// - the fields in our todoType maps with the json tags for the fields in our struct
+// - the field type matches the field type in our struct
+var todoType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Todo",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type: graphql.String,
+		},
+		"text": &graphql.Field{
+			Type: graphql.String,
+		},
+		"done": &graphql.Field{
+			Type: graphql.Boolean,
+		},
+	},
+})
+
 func SchemaConfig() graphql.SchemaConfig {
 	// Schema
 	fields := graphql.Fields{
@@ -11,6 +45,29 @@ func SchemaConfig() graphql.SchemaConfig {
 			Type: graphql.String,
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 				return "world", nil
+			},
+		},
+		"todo": &graphql.Field{
+			Type:        todoType,
+			Description: "Get single todo",
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+
+				idQuery, isOK := params.Args["id"].(string)
+				if isOK {
+					// Search for el with id
+					for _, todo := range TodoList {
+						if todo.ID == idQuery {
+							return todo, nil
+						}
+					}
+				}
+
+				return Todo{}, nil
 			},
 		},
 	}
