dbb49d06cc15add92be923f8d0b70b6b269ffb51
Ryan Brainard <ryan@ryanbrainard.com>
2019-06-26 17:17:22 +0900

    fix host

--- cmd/timesink-querier/main.go
+++ cmd/timesink-querier/main.go
@@ -17,7 +17,7 @@ func main() {
 	})
 
 	http.Handle("/graphql", h)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe("127.0.0.1:8080", nil); err != nil {
 		panic(err)
 	}
 }
